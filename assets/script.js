31c7fde8c9bd314590355e4f8d46a76e8bc15776
Senator O'Neil <danpatoneil@gmail.com>
2024-01-26 10:06:52 -0500

    added JavaScript loops to project

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,21 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-/* if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
-} */
-
-for(const topic of topics){
-    console.log(topic);
+function selectTopic(topic) {
+  if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+  } else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (topic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  }
 }
+function listTopics() {
+  for (const topic of topics) {
+    selectTopic(topic);
+  }
+}
+listTopics();
